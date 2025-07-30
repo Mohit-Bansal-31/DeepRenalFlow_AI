90ba28574d92e92b3cea2df9aa85d2be393fc94c
Mohit Bansal <mohitbansal0031@gmail.com>
2025-07-31 02:28:50 +0530

    create data ingestion pipeline

---
+++ src/DeepRenalFlow/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+# keeping the path to yaml files here as it is constant and will be used
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
